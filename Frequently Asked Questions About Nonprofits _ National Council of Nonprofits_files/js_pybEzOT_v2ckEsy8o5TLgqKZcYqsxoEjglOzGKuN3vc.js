531cef9af751e8b450ac5ba1de4d1bff4b0a3a09
Spandana Govindgari <spandana@Spandanas-MacBook-Pro.local>
2019-04-07 18:40:47 -0700

    Added dummy faq page

---
+++ Frequently Asked Questions About Nonprofits _ National Council of Nonprofits_files/js_pybEzOT_v2ckEsy8o5TLgqKZcYqsxoEjglOzGKuN3vc.js
@@ -0,0 +1,278 @@
+/*!
+ * Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php).
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ *
+ * $LastChangedDate: 2007-06-19 20:25:28 -0500 (Tue, 19 Jun 2007) $
+ * $Rev: 2111 $
+ *
+ * Version 2.1
+ */
+(function($){$.fn.bgIframe=$.fn.bgiframe=function(s){if($.browser.msie&&parseInt($.browser.version)<=6){s=$.extend({top:'auto',left:'auto',width:'auto',height:'auto',opacity:true,src:'javascript:false;'},s||{});var prop=function(n){return n&&n.constructor==Number?n+'px':n;},html='<iframe class="bgiframe"frameborder="0"tabindex="-1"src="'+s.src+'"'+'style="display:block;position:absolute;z-index:-1;'+(s.opacity!==false?'filter:Alpha(Opacity=\'0\');':'')+'top:'+(s.top=='auto'?'expression(((parseInt(this.parentNode.currentStyle.borderTopWidth)||0)*-1)+\'px\')':prop(s.top))+';'+'left:'+(s.left=='auto'?'expression(((parseInt(this.parentNode.currentStyle.borderLeftWidth)||0)*-1)+\'px\')':prop(s.left))+';'+'width:'+(s.width=='auto'?'expression(this.parentNode.offsetWidth+\'px\')':prop(s.width))+';'+'height:'+(s.height=='auto'?'expression(this.parentNode.offsetHeight+\'px\')':prop(s.height))+';'+'"/>';return this.each(function(){if($('> iframe.bgiframe',this).length==0)this.insertBefore(document.createElement(html),this.firstChild);});}return this;};if(!$.browser.version)$.browser.version=navigator.userAgent.toLowerCase().match(/.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/)[1];})(jQuery);;
+/*!
+* hoverIntent r5 // 2007.03.27 // jQuery 1.1.2+
+* <http://cherne.net/brian/resources/jquery.hoverIntent.html>
+* 
+* @param  f  onMouseOver function || An object with configuration options
+* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* @author    Brian Cherne <brian@cherne.net>
+*/
+(function($){$.fn.hoverIntent=function(f,g){var cfg={sensitivity:7,interval:100,timeout:0};cfg=$.extend(cfg,g?{over:f,out:g}:f);var cX,cY,pX,pY;var track=function(ev){cX=ev.pageX;cY=ev.pageY;};var compare=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);if((Math.abs(pX-cX)+Math.abs(pY-cY))<cfg.sensitivity){$(ob).unbind("mousemove",track);ob.hoverIntent_s=1;return cfg.over.apply(ob,[ev]);}else{pX=cX;pY=cY;ob.hoverIntent_t=setTimeout(function(){compare(ev,ob);},cfg.interval);}};var delay=function(ev,ob){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);ob.hoverIntent_s=0;return cfg.out.apply(ob,[ev]);};var handleHover=function(e){var p=(e.type=="mouseover"?e.fromElement:e.toElement)||e.relatedTarget;while(p&&p!=this){try{p=p.parentNode;}catch(e){p=this;}}if(p==this){return false;}var ev=jQuery.extend({},e);var ob=this;if(ob.hoverIntent_t){ob.hoverIntent_t=clearTimeout(ob.hoverIntent_t);}if(e.type=="mouseover"){pX=ev.pageX;pY=ev.pageY;$(ob).bind("mousemove",track);if(ob.hoverIntent_s!=1){ob.hoverIntent_t=setTimeout(function(){compare(ev,ob);},cfg.interval);}}else{$(ob).unbind("mousemove",track);if(ob.hoverIntent_s==1){ob.hoverIntent_t=setTimeout(function(){delay(ev,ob);},cfg.timeout);}}};return this.mouseover(handleHover).mouseout(handleHover);};})(jQuery);;
+;
+/*!
+ * Superfish v1.4.8 - jQuery menu widget
+ * Copyright (c) 2008 Joel Birch
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ * 	http://www.opensource.org/licenses/mit-license.php
+ * 	http://www.gnu.org/licenses/gpl.html
+ *
+ * CHANGELOG: http://users.tpg.com.au/j_birch/plugins/superfish/changelog.txt
+ */
+(function($){
+  $.fn.superfish = function(op){
+
+    var sf = $.fn.superfish,
+    c = sf.c,
+    $arrow = $(['<span class="',c.arrowClass,'"> &#187;</span>'].join('')),
+    over = function(){
+      var $$ = $(this), menu = getMenu($$);
+      clearTimeout(menu.sfTimer);
+      $$.showSuperfishUl().siblings().hideSuperfishUl();
+    },
+    out = function(){
+      var $$ = $(this), menu = getMenu($$), o = sf.op;
+      clearTimeout(menu.sfTimer);
+      menu.sfTimer = setTimeout(function(){
+        o.retainPath = ($.inArray($$[0],o.$path) > -1);
+        $$.hideSuperfishUl();
+        if (o.$path.length && $$.parents(['li.', o.hoverClass].join('')).length < 1){
+          over.call(o.$path);
+        }
+      }, o.delay);
+    },
+    getMenu = function($menu){
+      var menu = $menu.parents(['ul.',c.menuClass,':first'].join(''))[0];
+      sf.op = sf.o[menu.serial];
+      return menu;
+    },
+    addArrow = function($a){
+      $a.addClass(c.anchorClass).append($arrow.clone());
+    };
+
+    return this.each(function() {
+      var s = this.serial = sf.o.length;
+      var o = $.extend({},sf.defaults,op);
+      o.$path = $('li.' + o.pathClass,this).slice(0, o.pathLevels).each(function(){
+        $(this).addClass([o.hoverClass,c.bcClass].join(' '))
+        .filter('li:has(ul)').removeClass(o.pathClass);
+      });
+      sf.o[s] = sf.op = o;
+
+      $('li:has(ul)',this)[($.fn.hoverIntent && !o.disableHI) ? 'hoverIntent' : 'hover'](over,out).each(function() {
+        if (o.autoArrows) {
+          addArrow($('>a:first-child',this));
+        }
+      })
+      .not('.' + c.bcClass)
+      .hideSuperfishUl();
+
+      var $a = $('a',this);
+      $a.each(function(i){
+        var $li = $a.eq(i).parents('li');
+        $a.eq(i).focus(function(){
+          over.call($li);
+        }).blur(function(){
+          out.call($li);
+        });
+      });
+      o.onInit.call(this);
+
+    }).each(function() {
+      var menuClasses = [c.menuClass];
+      if (sf.op.dropShadows  && !($.browser.msie && $.browser.version < 7)) {
+        menuClasses.push(c.shadowClass);
+      }
+      $(this).addClass(menuClasses.join(' '));
+    });
+  };
+
+  var sf = $.fn.superfish;
+  sf.o = [];
+  sf.op = {};
+  sf.IE7fix = function(){
+    var o = sf.op;
+    if ($.browser.msie && $.browser.version > 6 && o.dropShadows && o.animation.opacity != undefined) {
+      this.toggleClass(sf.c.shadowClass + '-off');
+    }
+  };
+  sf.c = {
+    bcClass     : 'sf-breadcrumb',
+    menuClass   : 'sf-js-enabled',
+    anchorClass : 'sf-with-ul',
+    arrowClass  : 'sf-sub-indicator',
+    shadowClass : 'sf-shadow'
+  };
+  sf.defaults = {
+    hoverClass	: 'sfHover',
+    pathClass	: 'overideThisToUse',
+    pathLevels	: 1,
+    delay		: 800,
+    animation	: {
+      opacity:'show'
+    },
+    speed               : 'normal',
+    autoArrows	   : true,
+    dropShadows : true,
+    // true disables hoverIntent detection.
+    disableHI     : false,
+    onInit          : function(){}, // callback functions
+    onBeforeShow: function(){},
+    onShow          : function(){},
+    onHide          : function(){}
+  };
+  $.fn.extend({
+    hideSuperfishUl : function() {
+      var o = sf.op,
+      not = (o.retainPath === true) ? o.$path : '';
+      o.retainPath = false;
+      var $ul = $(['li.',o.hoverClass].join(''),this).add(this).not(not).removeClass(o.hoverClass)
+      .find('>ul').hide().css('visibility','hidden');
+      o.onHide.call($ul);
+      return this;
+    },
+    showSuperfishUl : function(){
+      var o = sf.op,
+      sh = sf.c.shadowClass + '-off',
+      $ul = this.addClass(o.hoverClass)
+      .find('>ul:hidden').css('visibility','visible');
+      sf.IE7fix.call($ul);
+      o.onBeforeShow.call($ul);
+      $ul.animate(o.animation,o.speed,function(){
+        sf.IE7fix.call($ul);
+        o.onShow.call($ul);
+      });
+      return this;
+    }
+  });
+
+})(jQuery);
+;
+/**
+ * @file
+ * Behaviours for the Nice Menus module. This uses Superfish 1.4.8.
+ *
+ * @link http://users.tpg.com.au/j_birch/plugins/superfish
+ */
+
+(function ($) {
+
+  /**
+   * Add Superfish to all Nice Menus with some basic options.
+   */
+  Drupal.behaviors.niceMenus = {
+    attach: function (context, settings) {
+      $('ul.nice-menu:not(.nice-menus-processed)').addClass('nice-menus-processed').each(function () {
+        $(this).superfish({
+          // Apply a generic hover class.
+          hoverClass: 'over',
+          // Disable generation of arrow mark-up.
+          autoArrows: false,
+          // Disable drop shadows.
+          dropShadows: false,
+          // Mouse delay.
+          delay: Drupal.settings.nice_menus_options.delay,
+          // Animation speed.
+          speed: Drupal.settings.nice_menus_options.speed
+        });
+
+        // Add in Brandon Aaron’s bgIframe plugin for IE select issues.
+        // http://plugins.jquery.com/node/46/release
+        $(this).find('ul').bgIframe({opacity:false});
+
+        $('ul.nice-menu ul').css('display', 'none');
+      });
+    }
+  };
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Retrieves the summary for the first element.
+ */
+$.fn.drupalGetSummary = function () {
+  var callback = this.data('summaryCallback');
+  return (this[0] && callback) ? $.trim(callback(this[0])) : '';
+};
+
+/**
+ * Sets the summary for all matched elements.
+ *
+ * @param callback
+ *   Either a function that will be called each time the summary is
+ *   retrieved or a string (which is returned each time).
+ */
+$.fn.drupalSetSummary = function (callback) {
+  var self = this;
+
+  // To facilitate things, the callback should always be a function. If it's
+  // not, we wrap it into an anonymous function which just returns the value.
+  if (typeof callback != 'function') {
+    var val = callback;
+    callback = function () { return val; };
+  }
+
+  return this
+    .data('summaryCallback', callback)
+    // To prevent duplicate events, the handlers are first removed and then
+    // (re-)added.
+    .unbind('formUpdated.summary')
+    .bind('formUpdated.summary', function () {
+      self.trigger('summaryUpdated');
+    })
+    // The actual summaryUpdated handler doesn't fire when the callback is
+    // changed, so we have to do this manually.
+    .trigger('summaryUpdated');
+};
+
+/**
+ * Sends a 'formUpdated' event each time a form element is modified.
+ */
+Drupal.behaviors.formUpdated = {
+  attach: function (context) {
+    // These events are namespaced so that we can remove them later.
+    var events = 'change.formUpdated click.formUpdated blur.formUpdated keyup.formUpdated';
+    $(context)
+      // Since context could be an input element itself, it's added back to
+      // the jQuery object and filtered again.
+      .find(':input').andSelf().filter(':input')
+      // To prevent duplicate events, the handlers are first removed and then
+      // (re-)added.
+      .unbind(events).bind(events, function () {
+        $(this).trigger('formUpdated');
+      });
+  }
+};
+
+/**
+ * Prepopulate form fields with information from the visitor cookie.
+ */
+Drupal.behaviors.fillUserInfoFromCookie = {
+  attach: function (context, settings) {
+    $('form.user-info-from-cookie').once('user-info-from-cookie', function () {
+      var formContext = this;
+      $.each(['name', 'mail', 'homepage'], function () {
+        var $element = $('[name=' + this + ']', formContext);
+        var cookie = $.cookie('Drupal.visitor.' + this);
+        if ($element.length && cookie) {
+          $element.val(cookie);
+        }
+      });
+    });
+  }
+};
+
+})(jQuery);
+;
